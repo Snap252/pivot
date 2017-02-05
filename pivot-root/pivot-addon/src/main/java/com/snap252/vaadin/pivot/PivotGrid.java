9631a694860db7f7efbcceea3da91eaadd71e7af
Snap252 <Snap252@Snap252-PC.fritz.box>
2017-02-05 14:44:38 +0100

    - should not have been committed/pushed

--- pivot-root/pivot-addon/src/main/java/com/snap252/vaadin/pivot/PivotGrid.java
+++ pivot-root/pivot-addon/src/main/java/com/snap252/vaadin/pivot/PivotGrid.java
@@ -1,13 +1,13 @@
 package com.snap252.vaadin.pivot;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.vaadin.treegrid.TreeGrid;
 
 import com.vaadin.data.Item;
 import com.vaadin.shared.ui.grid.ColumnResizeMode;
-import com.vaadin.ui.Grid;
 
 @NonNullByDefault
-public class PivotGrid extends Grid {
+public class PivotGrid extends TreeGrid {
 
 	public PivotGrid(final GridRendererParameter<Item> bucketParams) {
 		setPrimaryStyleName("v-grid-tiny");

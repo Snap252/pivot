40e88bdbc0d30963562e8884c90ef5546d69151c
schmidt <schmidt@entw16.sigma-gmbh.intern>
2017-03-17 13:49:02 +0100

    + I18N

--- pivot-core/pivot-core/src/main/java/com/snap252/org/pivoting/ShowingSubtotal.java
+++ pivot-core/pivot-core/src/main/java/com/snap252/org/pivoting/ShowingSubtotal.java
@@ -1,5 +1,11 @@
 package com.snap252.org.pivoting;
 
 public enum ShowingSubtotal {
-	AFTER, BEFORE, INHERIT, DONT_SHOW, BOTH
+	AFTER, BEFORE, INHERIT, DONT_SHOW, BOTH;
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return Messages.getString(getClass().getSimpleName() + "." + name());
+	}
 }

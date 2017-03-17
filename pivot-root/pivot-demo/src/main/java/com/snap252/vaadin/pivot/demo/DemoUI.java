8864810d55a1691052dcca7d4484c475168f2595
schmidt <schmidt@entw16.sigma-gmbh.intern>
2017-03-17 17:35:25 +0100

    + Locale Chooser in DemoUI

--- pivot-root/pivot-demo/src/main/java/com/snap252/vaadin/pivot/demo/DemoUI.java
+++ pivot-root/pivot-demo/src/main/java/com/snap252/vaadin/pivot/demo/DemoUI.java
@@ -1,6 +1,8 @@
 package com.snap252.vaadin.pivot.demo;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +23,7 @@ import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -47,12 +50,19 @@ public class DemoUI extends UI {
 
 		// Show it in the middle of the screen
 		final VerticalLayout layout = new VerticalLayout();
-		Button b = new Button(" DS ");
+		Button b = new Button("set DataSource");
 		b.addClickListener(i -> setDS(pivotGrid));
+
+		ComboBox loacleSelection = new ComboBox("Locale",
+				Arrays.asList(Locale.US, Locale.GERMANY, Locale.ITALY, Locale.ENGLISH));
+		loacleSelection.addValueChangeListener(evt -> {
+			getUI().setLocale((Locale) evt.getProperty().getValue());
+		});
+
 		layout.setStyleName("demoContentLayout");
 		layout.setSizeFull();
 		{
-			layout.addComponents(b, pivotGrid);
+			layout.addComponents(b, loacleSelection, pivotGrid);
 			layout.setExpandRatio(pivotGrid, 1);
 			// layout.setComponentAlignment(pivotGrid, Alignment.MIDDLE_CENTER);
 		}
